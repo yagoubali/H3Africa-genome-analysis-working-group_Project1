ef6a78b01085260a9d217a642c955569c40302a0
yagoubali <yagoubali@gmail.com>
2024-01-08 09:00:11 -0300

    bwa mem defualt k

---
+++ files/T2T_submit_defulatK.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+RESULT="/scratch3/users/yagoubali/project1_default_values"
+#SUCCEED_RUN=$(cat ${RESULT}/succeed_run.txt)
+SUBMISSION_LOG=$(date '+%Y-%m-%d')
+
+userId="yagoubali"
+baylor="/users/${userId}/H3A-Baylor_sample_list2.txt"	# only 10 samples
+cram_ids=($(cat ${baylor} | tr \\n " "))
+
+mkdir -p ${RESULT}
+###
+for cram_id in ${cram_ids[*]}; do 
+    echo "Submitting Job ${cram_id}"
+    mkdir -p /scratch3/users/${userId}/${cram_id}   
+	sbatch --job-name=${cram_id} \
+	       	       --output=/scratch3/users/${userId}/${cram_id}/${cram_id}_slurm_%j.out \
+	      	        /users/${userId}/T2T_run.slurm ${cram_id}
+	echo "${cram_id} submitted" >> ${RESULT}/Jobs_${SUBMISSION_LOG}.txt
+done 
