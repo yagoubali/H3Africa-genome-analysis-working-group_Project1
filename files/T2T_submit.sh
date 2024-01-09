98ede22164c59ec17acf425ef63b67dc7e7b1454
yagoubali <yagoubali@gmail.com>
2024-01-09 09:45:33 -0300

    use baylor's bwa mem parameters

--- files/T2T_submit.sh
+++ files/T2T_submit.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 
+
+userId="yagoubali"
 RESULT="/scratch3/users/yagoubali/project1_Y"
 outdir="/cbio/users/${userId}" 
 #SUCCEED_RUN=$(cat ${RESULT}/succeed_run.txt)
 SUBMISSION_LOG=$(date '+%Y-%m-%d')
 
-
-userId="yagoubali"
 baylor="/users/${userId}/H3A-Baylor_sample_list2.txt"	# only 10 samples
 cram_ids=($(cat ${baylor} | tr \\n " "))
 
