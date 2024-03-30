c6673d3d4c152b912f70c0c6ec26539e33b62ef7
yagoubali <yagoubali@gmail.com>
2024-03-30 08:32:22 -0300

    update adVNTR

--- stage2.3:run_adVNTR/Adam_adVNTR.sh
+++ stage2.3:run_adVNTR/Adam_adVNTR.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/bash
 
-userId="yagoubali" ## change this 
-subfolder="adam"   ## change this 
+userId="yagoubali"
+subfolder="adam"
 
 
-
-
-
-PROJECT="/cbio/projects/022"   ## This is a shared folder 
-scratch_dir="/scratch3/users/${userId}/advntr"   # This is your scratch3 folder
-outdir="/cbio/projects/022/${subfolder}/project1_all_samples/advntr_run"
+PROJECT="/cbio/projects/022"
+SCRATCH="/scratch3/users/${userId}/advntr"   # This is your scratch3 folder
+OUTDIR="${PROJECT}/${subfolder}/project1_all_samples/advntr_run"
 
 
 SUBMISSION_LOG=$(date '+%Y-%m-%d')
 
-baylor="/cbio/projects/022/adam/H3A-Baylor_sample_list.txt"	# all samples 346
+baylor="/cbio/projects/022/adam/H3A-Baylor_sample_list2.txt"	# all samples 346
 
 ## we store all bam ids in a bash array called bam_ids
 bam_ids=($(cat ${baylor} | tr \\n " "))
@@ -24,8 +21,9 @@ bam_ids=($(cat ${baylor} | tr \\n " "))
 ## -------> just in case we missed some information during the conversion process
 ##  ------> We converted cram files to fastq and there might be some reads have been lost.
 
-mkdir -p ${outdir}/hg38
-mkdir -p ${scratch_dir}
+mkdir -p ${OUTDIR}/hg38
+mkdir -p ${OUTDIR}/T2T
+mkdir -p ${SCRATCH}
 
 
 ## for loop will run pararell ananlyis for all 346 samples
@@ -34,8 +32,8 @@ for bam_id in ${bam_ids[*]}; do
       # here we run our slurm code using sbatch
         echo "Submitting Job ${bam_id}" 
         sbatch --job-name=${bam_id}   \
-                 --output=${scratch_dir}/${bam_id}_slurm_%j.out  \
+                 --output=${SCRATCH}/${bam_id}_slurm_%j.out  \
                  /users/${userId}/adVNTR.slurm  ${bam_id}
 
-    echo "${bam_id} submitted" >> ${scratch_dir}/Jobs_${SUBMISSION_LOG}.txt            
+    echo "${bam_id} submitted" >> ${SCRATCH}/Jobs_${SUBMISSION_LOG}.txt            
 done 
---
+++ stage2.3:run_adVNTR/README.md
@@ -0,0 +1,4 @@
+## Files to run
+
+> wget -c https://cseweb.ucsd.edu/~mbakhtia/adVNTR/vntr_data_recommended_loci_hg38.zip
+> unzip vntr_data_recommended_loci_hg38.zip
