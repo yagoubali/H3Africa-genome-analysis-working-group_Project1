535d42a6b80118b43768574867bfcff7f5d5e03a
yagoubali <yagoubali@gmail.com>
2024-07-11 07:48:01 -0300

    run HipSTR with T2T ref.

--- stage3_QC/Adam_HIPSTR_qc.sh
+++ stage3_QC/Adam_HIPSTR_qc.sh
@@ -6,8 +6,8 @@ subfolder="adam"
 
 
 PROJECT="/cbio/projects/022"
-SCRATCH="${PROJECT}/adam/project1_all_samples/HipSTR_qc"
-DATA="${PROJECT}/adam/project1_all_samples/HipSTR_sort"
+SCRATCH="/scratch3/users/${userId}/HipSTR_qc"
+DATA="${PROJECT}/adam/project1_all_samples/HipSTR_run"
 RESULT="${SCRATCH}/out"
 OUTDIR="${PROJECT}/${subfolder}/project1_all_samples"
 
@@ -18,8 +18,10 @@ SUBMISSION_LOG=$(date '+%Y-%m-%d')
 
 
 #baylor="/cbio/projects/022/adam/H3A-Baylor_sample_list.txt"
-baylor="/cbio/projects/022/adam/H3A-Baylor_sample_list.txt"	# all samples 346
-bam_ids=($(cat ${baylor} | tr \\n " "))
+#baylor="/cbio/projects/022/adam/H3A-Baylor_sample_list.txt"	# all samples 346
+#bam_ids=($(cat ${baylor} | tr \\n " "))
+
+bam_ids=("716-MALI"  "727-MALI"  "733-MALI"  "762-MALI")
 
 
 mkdir -p ${OUTDIR}/HipSTR_qc/T2T
@@ -28,7 +30,7 @@ mkdir -p ${SCRATCH}
 mkdir -p ${RESULT}
 for bam_id in ${bam_ids[*]}; do 
     echo "Submitting Job ${bam_id}" 
-	sbatch --job-name=${bam_id} \
+	sbatch --job-name=${bam_id}_qc \
 	       	       --output=${RESULT}/${bam_id}_slurm_%j.out \
 	      	        /users/${userId}/Adam_HipSTR_qc.slurm ${bam_id}
 	echo "${bam_id} submitted" >> ${RESULT}/Jobs_${SUBMISSION_LOG}.txt
